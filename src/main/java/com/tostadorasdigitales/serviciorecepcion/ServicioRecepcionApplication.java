6526bddcb7b781b04e6104197bfd7e630101449e
JuanVasquez01 <vasquezjuan772@gmail.com>
2024-10-12 16:05:48 -0500

    Initial commit

---
+++ src/main/java/com/tostadorasdigitales/serviciorecepcion/ServicioRecepcionApplication.java
@@ -0,0 +1,13 @@
+package com.tostadorasdigitales.serviciorecepcion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioRecepcionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioRecepcionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tostadorasdigitales/serviciorecepcion/ServicioRecepcionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tostadorasdigitales.serviciorecepcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioRecepcionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
